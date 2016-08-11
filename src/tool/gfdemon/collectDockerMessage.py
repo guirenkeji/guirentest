ac4962c42cd9b54793d7253cb42a4b7072e75292
fls <irelessqa.me@gmail.com>
2016-08-11 08:59:01 +0800

    add

--- src/tool/gfdemon/collectDockerMessage.py
+++ src/tool/gfdemon/collectDockerMessage.py
@@ -1,16 +1,18 @@
-
-
-
+import time
 import os
 import subprocess
-IP = os.environ['IP']
-# IP = '20.26.17.137:5000'
-# dockerimage = 'tomcattest:1.0'
-DOCKERIMAGE = os.environ['DOCKERIMAGE']
-dockerimage = DOCKERIMAGE
-BUILD_ID = os.environ['BUILD_ID']
+APP = os.environ['APPNAME']
+FROM = os.environ['FROM']
+CMD = os.environ['CMD']
+COPY = os.environ['COPY']
+IP = os.environ['IMAGESERVERIP']
+
+buidtime = time.strftime("%Y-%m-%d.%Hh%Mm%Ss",time.localtime(time.time()))
+BUILD_ID = buidtime
+BUILD_ID = BUILD_ID.encode()
 def collectDockerImageId ():
-    os.system ('docker build -t %s.%s . >readme' %(dockerimage,BUILD_ID))
+    modifyDockerfile()
+    os.system ('docker build -t %s/%s:%s . >readme' %(IP,APP,BUILD_ID))
     readmeFile = open('readme','r')
     readmeFilelist = readmeFile.readlines()
     content = readmeFilelist[-1]
@@ -18,12 +20,26 @@ def collectDockerImageId ():
     imageId=content[-1]
 #      os.system ('docker build -d tomcattest:1.0.2 .')
 #     os.system ('docker build -t tomcattest:1.0.%s . >>readme' %(BUILD_ID))
-    os.system ('docker tag %s %s/%s.%s' %(imageId,IP,dockerimage,BUILD_ID))
-    os.system ('docker push %s/%s.%s' %(IP,dockerimage,BUILD_ID))
+#     os.system ('docker tag %s %s/%s.%s' %(imageId,IP,dockerimage,BUILD_ID))
+    os.system ('docker push %s/%s:%s' %(IP,APP,BUILD_ID))
 #     os.system ('docker build -t tomcattest:1.0.%s .' %(BUILD_ID))
 #      subprocess.check_call('docker build -d tomcattest:1.0.1 .')
-    
+    images=IP+'/'+APP+':'+BUILD_ID
+    buildFile=open('build.txt','w')
+    buildFile.write(images)
+    buildFile.close()
 
+def modifyDockerfile():
+
+#         image = dockerimages
+#         copy= 'copy *.war /app/app/'
+#         COPY =copy
+#         FROM = 'FROM %s' %(image)
+#         CMD = ""
+        dockerfile ='%s\n%s\n%s' %(FROM,COPY,CMD)
+        buildFile=open('Dockerfile','w')
+        buildFile.write(dockerfile)
+        buildFile.close()
 
  
 run=collectDockerImageId()
--- src/tool/gfdemon/marathonapptask.py
+++ src/tool/gfdemon/marathonapptask.py
@@ -2,38 +2,43 @@ import os
 import json
 from marathon import MarathonClient
 from time import sleep
-IP = '20.26.17.137:5000'
-marathonip = 'http://20.26.17.133:8080'
-DOCKERIMAGE = os.environ['DOCKERIMAGE']
-dockerimage = DOCKERIMAGE
-dockerimage = IP+'/'+dockerimage
-BUILD_ID = os.environ['BUILD_ID']
-JOB_NAME = os.environ['JOB_NAME']
-SCALE=os.environ['SCALE']
+from jconfig import *
 
-dockerimage = dockerimage+'.'+BUILD_ID
-appname = JOB_NAME
-
-readed = json.load(open('temp.json', 'r'))
-print readed
-readed['container']['docker']['image'] = dockerimage
-readed['id'] = appname
-
-print readed
-
-json.dump(readed, open('app.json', 'w')) 
-c = MarathonClient(marathonip)
-
-try:
-   c.delete_app(appname,force=True)
-except :
-    pass
-  
-sleep(3) 
-
-# c.delete_app("test-2",force=True)
-os.system ('curl -X POST -H "Content-Type: application/json" %s/v2/apps -d@app.json' %(marathonip))
-
-SCALE = int(SCALE)
-c = MarathonClient('http://20.26.17.133:8080')
-c.scale_app(appname,SCALE,force=True)
+def servermarathon():
+        APP = os.environ['APPNAME']    
+#         STRING = os.environ['STRING']
+#         content = STRING
+#         contentlist = content.split('&')
+#         list = []
+#         for i in contentlist:
+#             p = i.split('=')
+#             p = p[1] 
+#             l = list.append(p)
+#         (maurl,mau,map) =tuple(list)
+#         marathonip = maurl
+#         user = mau
+#         password = map
+        c = MarathonClient(marathonip,username=user,password=password)
+        buildFile=open('build.txt','r')
+        dockerimage = buildFile.readline()
+        buildFile.close()
+        readed = json.load(open('temp.json', 'r'))
+        readed['container']['docker']['image'] = dockerimage
+        readed['id'] = APP
+        json.dump(readed, open('app.json', 'w')) 
+        
+        try:
+           c.delete_app(APP,force=True)
+           print 'delete'
+        except :
+            pass
+          
+        sleep(3)
+        u= user+':'+password
+        cmd1 = os.system ('curl -u %s -X POST -H "Content-Type: application/json" %s/v2/apps -d@app.json' %(u,marathonip))
+# SCALE =2
+# SCALE = int(SCALE)
+# c = MarathonClient(marathonip)
+# c.scale_app(name,SCALE,force=True)
+if __name__ == '__main__':
+    servermarathon()
